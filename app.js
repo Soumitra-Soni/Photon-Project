951281be4ba05b7a2dff9efb48c2b71007bcf5d3
Soumitra-Soni <somitrasoni123@gmail.com>
2023-05-20 17:45:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const auth = "opF6QYl4mMBhQfXkHKcs3eYnbX6cV538JOMNjNZoHxH8jzCG4oBxpi7f";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+let searchValue;
+const more = document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
+
+//event listeners
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  currentSearch = searchValue;
+  searchPhotos(searchValue);
+});
+more.addEventListener("click", loadMore);
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    methord: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  return data;
+}
+
+function genaratePictures(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `
+    <div class="gallery-info">
+    <p>${photo.photographer}</p>
+    <a href=${photo.src.original}>Download</a>
+    <img src=${photo.src.large}></img>
+    </div>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function curatedPhotos() {
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
+  genaratePictures(data);
+}
+
+async function searchPhotos(query) {
+  clear();
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
+  genaratePictures(data);
+}
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  genaratePictures(data);
+}
+curatedPhotos();
